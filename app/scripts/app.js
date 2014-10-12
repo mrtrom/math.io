47985a39fbf6593660aadd2ca102ffee06816ba6
Alexander Ochoa <ochoaseguralex@gmail.com>
2014-10-12 12:44:52 -0500

    interceptors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,43 +8,51 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('Mathio', ['ionic', 'config'])
 
-    .run(function($ionicPlatform) {
-      $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-        // for form inputs)
-        if(window.cordova && window.cordova.plugins.Keyboard) {
-          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if(window.StatusBar) {
-          // org.apache.cordova.statusbar required
-          StatusBar.styleDefault();
-        }
-      });
+    .run(function($ionicPlatform, $rootScope, $state, $urlRouter) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+        });
+        $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+            //Ionic - Angular Interceptor
+        });
     })
 
-    .config(function($stateProvider, $urlRouterProvider) {
-      // Ionic uses AngularUI Router which uses the concept of states
-      // Learn more here: https://github.com/angular-ui/ui-router
-      // Set up the various states which the app can be in.
-      // Each state's controller can be found in controllers.js
-      $stateProvider
+    .config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
-        // setup an abstract state for the tabs directive
-          .state('app', {
-            url: '/app',
-            abstract: true,
-            templateUrl: 'templates/application/menu.html'
-          })
-          .state('app.home', {
-            url: '/home',
-            views: {
-              'homeContent' :{
-                templateUrl: 'templates/home/home.html',
-                controller: 'HomeCtrl'
-              }
-            }
-          });
-      // if none of the above states are matched, use this as the fallback
-      $urlRouterProvider.otherwise('/app/home');
+            // setup an abstract state for the tabs directive
+            .state('app', {
+                url: '/app',
+                abstract: true,
+                templateUrl: 'templates/application/menu.html'
+            })
+            .state('app.home', {
+                url: '/home',
+                views: {
+                    'homeContent' :{
+                        templateUrl: 'templates/home/home.html',
+                        controller: 'HomeCtrl'
+                    }
+                }
+            });
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/app/home');
+
+        $httpProvider.interceptors.push(['$q', '$location','$injector', function($q, $location , $injector) {
+            return {
+                'responseError': function(response) {
+                    //HTTP Interceptor
+                    return $q.reject(response);
+                }
+            };
+        }]);
     });
 
