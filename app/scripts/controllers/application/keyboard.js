d58aa360c9ab3d2f7d2aaec6eb917b412098a3a0
mrtrom <ochoaseguralex@gmail.com>
2014-10-13 14:25:47 -0500

    keyboard update

--- app/scripts/controllers/application/keyboard.js
+++ app/scripts/controllers/application/keyboard.js
@@ -4,5 +4,8 @@ angular.module('Mathio')
     .controller('AppKeyboardCtrl', function($scope) {
       $scope.writeOnTop = function(element){
 
-      }
+      };
+      $scope.solveProblem = function(){
+
+      };
     });
